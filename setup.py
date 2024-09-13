1d3805cca3f82fe4b5cee54e46d4970395a95ec7
jakobap <jakobpoerschmann@gmail.com>
2024-09-13 16:11:37 +0200

    graph2sql dependencies

--- setup.py
+++ setup.py
@@ -4,4 +4,13 @@ setup(
     name='graph2nosql',  # Choose a name for your library
     version='0.1', 
     packages=find_packages(),
+    install_requires=[
+        'networkx==3.3',
+        'matplotlib==3.9.1',
+        'langfuse==2.39.2',
+        'graspologic==3.4.1',
+        'numpy==2.0.1',
+        'firebase-admin==6.5.0',
+        'python-dotenv==1.0.0'
+    ]
 )
