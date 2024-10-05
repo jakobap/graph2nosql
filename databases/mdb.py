0b4d12bceb00696db4d8e6744eb4ec0ae3242ce0
jakoap <jakobpoerschmann@gmail.com>
2024-10-05 21:03:31 +0000

    mdb missing directed attributes in NodeData

--- databases/mdb.py
+++ databases/mdb.py
@@ -8,11 +8,12 @@ from pymongo.server_api import ServerApi
 from datamodel.data_model import NodeData, EdgeData, CommunityData
 from base.operations import NoSQLKnowledgeGraph
 
-import networkx as nx # type: ignore
+import networkx as nx  # type: ignore
 
 
 class MongoKG(NoSQLKnowledgeGraph):
     """MongoDB Database Operations Class"""
+
     def __init__(self,
                  mdb_uri: str,
                  mdb_db_id: str,
@@ -26,9 +27,9 @@ class MongoKG(NoSQLKnowledgeGraph):
         self.mdb_client = MongoClient(str(mdb_uri), server_api=ServerApi('1'))
 
         self.db = self.mdb_client[mdb_db_id]
-        self.mdb_node_coll = self.db[node_coll_id] 
-        self.mdbe_edges_coll = self.db[edges_coll_id] 
-        self.mdb_comm_coll = self.db[community_collection_id] 
+        self.mdb_node_coll = self.db[node_coll_id]
+        self.mdbe_edges_coll = self.db[edges_coll_id]
+        self.mdb_comm_coll = self.db[community_collection_id]
 
         try:
             # client.admin.command('ping')
@@ -43,7 +44,8 @@ class MongoKG(NoSQLKnowledgeGraph):
         try:
             # Check if a node with the same node_uid already exists
             if self.mdb_node_coll.find_one({"node_uid": node_uid}):
-                raise ValueError(f"Error: Node with node_uid '{node_uid}' already exists.")
+                raise ValueError(
+                    f"Error: Node with node_uid '{node_uid}' already exists.")
 
             # Convert NodeData to a dictionary for MongoDB storage
             node_data_dict = node_data.__dict__
@@ -52,7 +54,8 @@ class MongoKG(NoSQLKnowledgeGraph):
             self.mdb_node_coll.insert_one(node_data_dict)
 
         except Exception as e:
-            raise Exception(f"Error adding node with node_uid '{node_uid}': {e}") from e
+            raise Exception(
+                f"Error adding node with node_uid '{node_uid}': {e}") from e
 
     def get_node(self, node_uid: str) -> NodeData:
         """Retrieves an node from the knowledge graph."""
@@ -68,10 +71,10 @@ class MongoKG(NoSQLKnowledgeGraph):
                 node_description=node_data_dict['node_description'],
                 node_degree=node_data_dict.get('node_degree', 0),
                 document_id=node_data_dict.get('document_id', ''),
-                community_id=node_data_dict.get('community_id', ''),  
-                edges_to=node_data_dict.get('edges_to', []),     
-                edges_from=node_data_dict.get('edges_from', []),  
-                embedding=node_data_dict.get('embedding', [])     
+                community_id=node_data_dict.get('community_id', ''),
+                edges_to=node_data_dict.get('edges_to', []),
+                edges_from=node_data_dict.get('edges_from', []),
+                embedding=node_data_dict.get('embedding', [])
             )
         else:
             raise KeyError(f"Error: No node found with node_uid: {node_uid}")
@@ -81,7 +84,8 @@ class MongoKG(NoSQLKnowledgeGraph):
         try:
             # Check if the node exists
             if not self.mdb_node_coll.find_one({"node_uid": node_uid}):
-                raise KeyError(f"Error: Node with node_uid '{node_uid}' does not exist.")
+                raise KeyError(
+                    f"Error: Node with node_uid '{node_uid}' does not exist.")
 
             # Convert NodeData to a dictionary for MongoDB storage
             node_data_dict = node_data.__dict__
@@ -92,14 +96,16 @@ class MongoKG(NoSQLKnowledgeGraph):
             )
 
         except Exception as e:
-            raise Exception(f"Error updating node with node_uid '{node_uid}': {e}") from e
+            raise Exception(
+                f"Error updating node with node_uid '{node_uid}': {e}") from e
 
     def remove_node(self, node_uid: str) -> None:
         """Removes a node from the knowledge graph."""
 
         # Check if the node exists
         if not self.node_exist(node_uid=node_uid):
-            raise KeyError(f"Error: Node with node_uid '{node_uid}' does not exist.")
+            raise KeyError(
+                f"Error: Node with node_uid '{node_uid}' does not exist.")
 
         # 1. Get the node data to find its connections
         node_data = self.get_node(node_uid)
@@ -140,51 +146,64 @@ class MongoKG(NoSQLKnowledgeGraph):
     def add_edge(self, edge_data: EdgeData) -> None:
         """Adds an edge (relationship) between two entities in the knowledge graph."""
 
-        #TODO: consider moving to base class.
-        
+        # TODO: consider moving to base class.
+
         # Check if source and target nodes exist
-        if not self.get_node(edge_data.source_uid):
-            raise KeyError(f"Error: Source node with node_uid '{edge_data.source_uid}' does not exist.")
-        if not self.get_node(edge_data.target_uid):
-            raise KeyError(f"Error: Target node with node_uid '{edge_data.target_uid}' does not exist.")
+        if not self.node_exist(edge_data.source_uid):
+            raise KeyError(
+                f"Error: Source node with node_uid '{edge_data.source_uid}' does not exist.")
+        if not self.node_exist(edge_data.target_uid):
+            raise KeyError(
+                f"Error: Target node with node_uid '{edge_data.target_uid}' does not exist.")
 
         # Type checking for edge_data
         if not isinstance(edge_data, EdgeData):
-            raise TypeError(f"Error: edge_data must be of type EdgeData, not {type(edge_data)}")
+            raise TypeError(
+                f"Error: edge_data must be of type EdgeData, not {type(edge_data)}")
 
-        edge_uid = self._generate_edge_uid(edge_data.source_uid, edge_data.target_uid)
+        edge_uid = self._generate_edge_uid(
+            edge_data.source_uid, edge_data.target_uid)
 
         try:
             source_node_data = self.get_node(edge_data.source_uid)
             target_node_data = self.get_node(edge_data.target_uid)
 
-            source_node_data.edges_to = list(set(source_node_data.edges_to) | {edge_data.target_uid})
+            source_node_data.edges_to = list(
+                set(source_node_data.edges_to) | {edge_data.target_uid})
             self.update_node(edge_data.source_uid, source_node_data)
 
             # Add the edge to the target node's edges_from
-            target_node_data.edges_from = list(set(target_node_data.edges_from) | {edge_data.source_uid})
+            target_node_data.edges_from = list(
+                set(target_node_data.edges_from) | {edge_data.source_uid})
             self.update_node(edge_data.target_uid, target_node_data)
 
             # Add the edge to the edges collection
             self._update_egde_coll(edge_uid=edge_uid,
-                       target_uid=edge_data.target_uid,
-                       source_uid=edge_data.source_uid,
-                       description=edge_data.description)
+                                   target_uid=edge_data.target_uid,
+                                   source_uid=edge_data.source_uid,
+                                   description=edge_data.description,
+                                   directed=edge_data.directed)
 
             if not edge_data.directed:  # If undirected, add the reverse edge as well
-                target_node_data.edges_to = list(set(target_node_data.edges_to) | {edge_data.source_uid})
+                reverse_edge_uid = self._generate_edge_uid(
+                    edge_data.target_uid, edge_data.source_uid)
+
+                target_node_data.edges_to = list(
+                    set(target_node_data.edges_to) | {edge_data.source_uid})
                 self.update_node(edge_data.target_uid, target_node_data)
 
                 # Since it's undirected, also add source_uid to target_node_data.edges_from
-                source_node_data.edges_from = list(set(source_node_data.edges_from) | {edge_data.target_uid})
+                source_node_data.edges_from = list(
+                    set(source_node_data.edges_from) | {edge_data.target_uid})
                 self.update_node(edge_data.source_uid, source_node_data)
 
                 # Add the reverse edge to the edges collection
-                self._update_egde_coll(edge_uid=edge_uid,
+                self._update_egde_coll(edge_uid=reverse_edge_uid,
                                        target_uid=edge_data.source_uid,
                                        source_uid=edge_data.target_uid,
-                                       description=edge_data.description)
-                                       
+                                       description=edge_data.description,
+                                       directed=edge_data.directed)
+
         except ValueError as e:
             raise ValueError(
                 f"Error: Could not add edge from '{edge_data.source_uid}' to '{edge_data.target_uid}'. Details: {e}"
@@ -200,7 +219,8 @@ class MongoKG(NoSQLKnowledgeGraph):
                 edge_uid=edge_data_dict.get('edge_uid', ''),
                 source_uid=edge_data_dict.get('source_uid', ''),
                 target_uid=edge_data_dict.get('target_uid', ''),
-                description=edge_data_dict.get('description', '')
+                description=edge_data_dict.get('description', ''),
+                directed=edge_data_dict.get('directed', True)
             )
         else:
             raise KeyError(f"Error: No edge found with edge_uid: {edge_uid}")
@@ -212,12 +232,15 @@ class MongoKG(NoSQLKnowledgeGraph):
 
         # 1. Validate input and check if the edge exists
         if not isinstance(edge_data, EdgeData):
-            raise TypeError(f"Error: edge_data must be of type EdgeData, not {type(edge_data)}")
+            raise TypeError(
+                f"Error: edge_data must be of type EdgeData, not {type(edge_data)}")
 
-        edge_uid = self._generate_edge_uid(edge_data.source_uid, edge_data.target_uid)
-        
+        edge_uid = self._generate_edge_uid(
+            edge_data.source_uid, edge_data.target_uid)
+
         if not self.edge_exist(source_uid=edge_data.source_uid, target_uid=edge_data.target_uid):
-            raise KeyError(f"Error: Edge with edge_uid '{edge_uid}' does not exist.")
+            raise KeyError(
+                f"Error: Edge with edge_uid '{edge_uid}' does not exist.")
 
         # 2. Update the edge document in the EDGES collection
         try:
@@ -225,10 +248,12 @@ class MongoKG(NoSQLKnowledgeGraph):
                 edge_uid=edge_uid,
                 target_uid=edge_data.target_uid,
                 source_uid=edge_data.source_uid,
-                description=edge_data.description
+                description=edge_data.description,
+                directed=edge_data.directed
             )
         except Exception as e:
-            raise Exception(f"Error updating edge in edges collection: {e}") from e
+            raise Exception(
+                f"Error updating edge in edges collection: {e}") from e
 
         # 3. Update edge references in the NODES collection
         try:
@@ -236,25 +261,29 @@ class MongoKG(NoSQLKnowledgeGraph):
             source_node_data = self.get_node(edge_data.source_uid)
             # Ensure the target_uid is present in edges_to
             if edge_data.target_uid not in source_node_data.edges_to:
-                source_node_data.edges_to = list(set(source_node_data.edges_to) | {edge_data.target_uid})
+                source_node_data.edges_to = list(
+                    set(source_node_data.edges_to) | {edge_data.target_uid})
                 self.update_node(edge_data.source_uid, source_node_data)
 
             # 3b. Update target node
             target_node_data = self.get_node(edge_data.target_uid)
             # Ensure the source_uid is present in edges_from
             if edge_data.source_uid not in target_node_data.edges_from:
-                target_node_data.edges_from = list(set(target_node_data.edges_from) | {edge_data.source_uid})
+                target_node_data.edges_from = list(
+                    set(target_node_data.edges_from) | {edge_data.source_uid})
                 self.update_node(edge_data.target_uid, target_node_data)
 
         except Exception as e:
-            raise Exception(f"Error updating edge references in nodes: {e}") from e
+            raise Exception(
+                f"Error updating edge references in nodes: {e}") from e
 
     def remove_edge(self, source_uid: str, target_uid: str) -> None:
         """Removes an edge between two entities."""
 
         # Get involved edge and node data
         try:
-            edge_data = self.get_edge(source_uid=source_uid, target_uid=target_uid)
+            edge_data = self.get_edge(
+                source_uid=source_uid, target_uid=target_uid)
         except Exception as e:
             raise Exception(f"Error getting edge: {e}") from e
 
@@ -273,20 +302,23 @@ class MongoKG(NoSQLKnowledgeGraph):
             source_node_data.edges_to.remove(target_uid)
             self.update_node(source_uid, source_node_data)
         except ValueError as e:
-            raise ValueError(f"Error: Target node not in source's edges_to: {e}")
+            raise ValueError(
+                f"Error: Target node not in source's edges_to: {e}")
 
-        # remove source_uid from target <- source 
+        # remove source_uid from target <- source
         try:
             target_node_data.edges_from.remove(source_uid)
             self.update_node(target_uid, target_node_data)
         except ValueError as e:
-            raise ValueError(f"Error: Source node not in target's edges_to: {e}")
+            raise ValueError(
+                f"Error: Source node not in target's edges_to: {e}")
 
         # Remove the edge from the edges collection
         edge_uid = self._generate_edge_uid(source_uid, target_uid)
         delete_result = self.mdbe_edges_coll.delete_one({"edge_uid": edge_uid})
         if delete_result.deleted_count == 0:
-            raise KeyError(f"Error: No edge found with source_uid '{source_uid}' and target_uid '{target_uid}'")
+            raise KeyError(
+                f"Error: No edge found with source_uid '{source_uid}' and target_uid '{target_uid}'")
 
         # remove the opposite direction if edge undirected
         if not edge_data.directed:
@@ -295,20 +327,24 @@ class MongoKG(NoSQLKnowledgeGraph):
                 source_node_data.edges_from.remove(target_uid)
                 self.update_node(source_uid, source_node_data)
             except ValueError as e:
-                raise ValueError(f"Error: Target node not in source's edges_to: {e}")
-            
+                raise ValueError(
+                    f"Error: Target node not in source's edges_to: {e}")
+
             # remove source_uid from target -> source
             try:
                 target_node_data.edges_to.remove(source_uid)
                 self.update_node(target_uid, target_node_data)
             except ValueError as e:
-                raise ValueError(f"Error: Source node not in target's edges_to: {e}")
+                raise ValueError(
+                    f"Error: Source node not in target's edges_to: {e}")
 
             # Remove the edge from the edges collection
             reverse_edge_uid = self._generate_edge_uid(target_uid, source_uid)
-            delete_result = self.mdbe_edges_coll.delete_one({"edge_uid": reverse_edge_uid})
+            delete_result = self.mdbe_edges_coll.delete_one(
+                {"edge_uid": reverse_edge_uid})
             if delete_result.deleted_count == 0:
-                raise KeyError(f"Error: No reverse edge found with source_uid '{target_uid}' and target_uid '{source_uid}'")
+                raise KeyError(
+                    f"Error: No reverse edge found with source_uid '{target_uid}' and target_uid '{source_uid}'")
 
         else:
             pass
@@ -340,13 +376,15 @@ class MongoKG(NoSQLKnowledgeGraph):
     def _generate_edge_uid(self, source_uid: str, target_uid: str):
         return f"{source_uid}_to_{target_uid}"
 
-    def _update_egde_coll(self, edge_uid: str, source_uid: str, target_uid: str, description: str) -> None:
+    def _update_egde_coll(self, edge_uid: str, source_uid: str,
+                          target_uid: str, description: str, directed: bool) -> None:
         """Update edge record in the edges collection."""
         edge_data_dict = {
             "edge_uid": edge_uid,
             "source_uid": source_uid,
             "target_uid": target_uid,
-            "description": description
+            "description": description,
+            "directed": directed
         }
         self.mdbe_edges_coll.update_one(
             {"edge_uid": edge_uid}, {"$set": edge_data_dict}, upsert=True
@@ -380,7 +418,7 @@ class MongoKG(NoSQLKnowledgeGraph):
         """Checks for node existence and returns boolean"""
         if self.mdb_node_coll.find_one({"node_uid": node_uid}) is not None:
             return True
-        else: 
+        else:
             return False
 
     def flush_kg(self) -> None:
@@ -412,7 +450,7 @@ if __name__ == "__main__":
     mdb_cluster = str(secrets["MDB_CLUSTER"])
 
     uri = f"mongodb+srv://{mdb_username}:{mdb_passowrd}@cluster0.pjx3w.mongodb.net/?retryWrites=true&w=majority&appName={mdb_cluster}"
-    
+
     mkg = MongoKG(
         mdb_uri=uri,
         mdb_db_id=str(secrets["MDB_DB_ID"]),
